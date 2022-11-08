da6b9b2c4bd2e26b71d783c066ec2be83029e665
Anil Yavuz <anielyavuz@gmail.com>
2022-11-08 23:44:29 +0300

    first commit

---
+++ lib/pages/intro.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Center(child: Text("Giris ekrani")),
+        ),
+      ),
+    );
+  }
+}
