6d9d9974b2a9995a4aa6bf6a80ca836abc0d613d
mutlucanunal <mutlucan.unal@gmail.com>
2022-11-18 01:24:53 +0300

    01:24 18.11.2022

--- lib/pages/chooseyourhabits.dart
+++ lib/pages/chooseyourhabits.dart
@@ -22,26 +22,68 @@ class _ChooseHabitsState extends State<ChooseHabits> {
     return Scaffold(
         backgroundColor: Color.fromRGBO(21, 9, 35, 1),
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                "Choose Your Habits",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: _yaziTipiRengi,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Times New Roman',
-                  // fontWeight: FontWeight.bold
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Choose Your Habits",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: _yaziTipiRengi,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Times New Roman',
+                    // fontWeight: FontWeight.bold
+                  ),
                 ),
-              ),
-              Container(
-                  color: Colors.red.shade100,
+                Container(
+                    color: Colors.red.shade100,
+                    child: ExpandableNotifier(
+                        child: ScrollOnExpand(
+                            child: ExpandablePanel(
+                      header: Text("Health",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: Color.fromRGBO(21, 9, 35, 1),
+                            fontSize: 25,
+                            fontFamily: 'Times New Roman',
+                            // fontWeight: FontWeight.bold
+                          )),
+                      theme: ExpandableThemeData(iconColor: Colors.blue),
+                      expanded: HabitGroup2(habitList: [
+                        "Meditation",
+                        "Yoga",
+                        "Drink Water",
+                        "Sleep Well"
+                      ], yaziTipiRengi: _yaziTipiRengi),
+                      collapsed: Container(),
+                    )))),
+                Container(
                   child: ExpandableNotifier(
                       child: ScrollOnExpand(
                           child: ExpandablePanel(
-                    header: Text("Health",
+                    header: Text("Sport",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          color: Color.fromRGBO(21, 9, 35, 1),
+                          fontSize: 25,
+                          fontFamily: 'Times New Roman',
+                          // fontWeight: FontWeight.bold
+                        )),
+                    theme: ExpandableThemeData(iconColor: Colors.blue),
+                    expanded: HabitGroup2(
+                        habitList: ["Walk", "Run", "Swim", "Push Up"],
+                        yaziTipiRengi: _yaziTipiRengi),
+                    collapsed: Container(),
+                  ))),
+                  color: Colors.red.shade200,
+                ),
+                Container(
+                  child: ExpandableNotifier(
+                      child: ScrollOnExpand(
+                          child: ExpandablePanel(
+                    header: Text("Study",
                         textAlign: TextAlign.start,
                         style: TextStyle(
                           color: Color.fromRGBO(21, 9, 35, 1),
@@ -51,163 +93,123 @@ class _ChooseHabitsState extends State<ChooseHabits> {
                         )),
                     theme: ExpandableThemeData(iconColor: Colors.blue),
                     expanded: HabitGroup2(habitList: [
-                      "Meditation",
-                      "Yoga",
-                      "Drink Water",
-                      "Sleep Well"
+                      "Read a book",
+                      "Learn English",
+                      "Math Exercise",
+                      "Law"
                     ], yaziTipiRengi: _yaziTipiRengi),
                     collapsed: Container(),
-                  )))),
-              Container(
-                child: ExpandableNotifier(
-                    child: ScrollOnExpand(
-                        child: ExpandablePanel(
-                  header: Text("Sport",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Color.fromRGBO(21, 9, 35, 1),
-                        fontSize: 25,
-                        fontFamily: 'Times New Roman',
-                        // fontWeight: FontWeight.bold
-                      )),
-                  theme: ExpandableThemeData(iconColor: Colors.blue),
-                  expanded: HabitGroup2(
-                      habitList: ["Walk", "Run", "Swim", "Push Up"],
-                      yaziTipiRengi: _yaziTipiRengi),
-                  collapsed: Container(),
-                ))),
-                color: Colors.red.shade200,
-              ),
-              Container(
-                child: ExpandableNotifier(
-                    child: ScrollOnExpand(
-                        child: ExpandablePanel(
-                  header: Text("Study",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Color.fromRGBO(21, 9, 35, 1),
-                        fontSize: 25,
-                        fontFamily: 'Times New Roman',
-                        // fontWeight: FontWeight.bold
-                      )),
-                  theme: ExpandableThemeData(iconColor: Colors.blue),
-                  expanded: HabitGroup2(habitList: [
-                    "Read a book",
-                    "Learn English",
-                    "Math Exercise",
-                    "Law"
-                  ], yaziTipiRengi: _yaziTipiRengi),
-                  collapsed: Container(),
-                ))),
-                color: Colors.red.shade300,
-              ),
-              Container(
-                child: ExpandableNotifier(
-                    child: ScrollOnExpand(
-                        child: ExpandablePanel(
-                  header: Text("Art",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Color.fromRGBO(21, 9, 35, 1),
-                        fontSize: 25,
-                        fontFamily: 'Times New Roman',
-                        // fontWeight: FontWeight.bold
-                      )),
-                  theme: ExpandableThemeData(iconColor: Colors.blue),
-                  expanded: HabitGroup2(habitList: [
-                    "Play Guitar",
-                    "Painting",
-                    "Play Piano",
-                    "Dance"
-                  ], yaziTipiRengi: _yaziTipiRengi),
-                  collapsed: Container(),
-                ))),
-                color: Colors.red.shade400,
-              ),
-              Container(
-                child: ExpandableNotifier(
-                    child: ScrollOnExpand(
-                        child: ExpandablePanel(
-                  header: Text("Finance",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Color.fromRGBO(21, 9, 35, 1),
-                        fontSize: 25,
-                        fontFamily: 'Times New Roman',
-                        // fontWeight: FontWeight.bold
-                      )),
-                  theme: ExpandableThemeData(iconColor: Colors.blue),
-                  expanded: HabitGroup2(habitList: [
-                    "Saving Money",
-                    "Investing",
-                    "Donation",
-                    "Market Search"
-                  ], yaziTipiRengi: _yaziTipiRengi),
-                  collapsed: Container(),
-                ))),
-                color: Colors.red.shade500,
-              ),
-              Container(
-                child: ExpandableNotifier(
-                    child: ScrollOnExpand(
-                        child: ExpandablePanel(
-                  header: Text("Social",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Color.fromRGBO(21, 9, 35, 1),
-                        fontSize: 25,
-                        fontFamily: 'Times New Roman',
-                        // fontWeight: FontWeight.bold
-                      )),
-                  theme: ExpandableThemeData(iconColor: Colors.blue),
-                  expanded: HabitGroup2(habitList: [
-                    "Cinema",
-                    "Meet with friends",
-                    "Theater",
-                    "Listen Podcast"
-                  ], yaziTipiRengi: _yaziTipiRengi),
-                  collapsed: Container(),
-                ))),
-                color: Colors.red.shade600,
-              ),
-              Container(
-                child: ExpandableNotifier(
-                    child: ScrollOnExpand(
-                        child: ExpandablePanel(
-                  header: Text("Quit a Bad Habit",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Color.fromRGBO(21, 9, 35, 1),
-                        fontSize: 25,
-                        fontFamily: 'Times New Roman',
-                        // fontWeight: FontWeight.bold
-                      )),
-                  theme: ExpandableThemeData(iconColor: Colors.blue),
-                  expanded: HabitGroup2(habitList: [
-                    "Quit smoking",
-                    "Quit eating snacks",
-                    "Quit alcohol",
-                    "Stop swearing"
-                  ], yaziTipiRengi: _yaziTipiRengi),
-                  collapsed: Container(),
-                ))),
-                color: Colors.red.shade700,
-              ),
-              Center(
-                child: InkWell(
-                  onTap: () async {
-                    await _auth.signOut();
-
-                    var a = await _authService.signOut();
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => CheckAuth()));
-                  },
-                  child: Container(
-                    child: Text("Sign Out"),
+                  ))),
+                  color: Colors.red.shade300,
+                ),
+                Container(
+                  child: ExpandableNotifier(
+                      child: ScrollOnExpand(
+                          child: ExpandablePanel(
+                    header: Text("Art",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          color: Color.fromRGBO(21, 9, 35, 1),
+                          fontSize: 25,
+                          fontFamily: 'Times New Roman',
+                          // fontWeight: FontWeight.bold
+                        )),
+                    theme: ExpandableThemeData(iconColor: Colors.blue),
+                    expanded: HabitGroup2(habitList: [
+                      "Play Guitar",
+                      "Painting",
+                      "Play Piano",
+                      "Dance"
+                    ], yaziTipiRengi: _yaziTipiRengi),
+                    collapsed: Container(),
+                  ))),
+                  color: Colors.red.shade400,
+                ),
+                Container(
+                  child: ExpandableNotifier(
+                      child: ScrollOnExpand(
+                          child: ExpandablePanel(
+                    header: Text("Finance",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          color: Color.fromRGBO(21, 9, 35, 1),
+                          fontSize: 25,
+                          fontFamily: 'Times New Roman',
+                          // fontWeight: FontWeight.bold
+                        )),
+                    theme: ExpandableThemeData(iconColor: Colors.blue),
+                    expanded: HabitGroup2(habitList: [
+                      "Saving Money",
+                      "Investing",
+                      "Donation",
+                      "Market Search"
+                    ], yaziTipiRengi: _yaziTipiRengi),
+                    collapsed: Container(),
+                  ))),
+                  color: Colors.red.shade500,
+                ),
+                Container(
+                  child: ExpandableNotifier(
+                      child: ScrollOnExpand(
+                          child: ExpandablePanel(
+                    header: Text("Social",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          color: Color.fromRGBO(21, 9, 35, 1),
+                          fontSize: 25,
+                          fontFamily: 'Times New Roman',
+                          // fontWeight: FontWeight.bold
+                        )),
+                    theme: ExpandableThemeData(iconColor: Colors.blue),
+                    expanded: HabitGroup2(habitList: [
+                      "Cinema",
+                      "Meet with friends",
+                      "Theater",
+                      "Listen Podcast"
+                    ], yaziTipiRengi: _yaziTipiRengi),
+                    collapsed: Container(),
+                  ))),
+                  color: Colors.red.shade600,
+                ),
+                Container(
+                  child: ExpandableNotifier(
+                      child: ScrollOnExpand(
+                          child: ExpandablePanel(
+                    header: Text("Quit a Bad Habit",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          color: Color.fromRGBO(21, 9, 35, 1),
+                          fontSize: 25,
+                          fontFamily: 'Times New Roman',
+                          // fontWeight: FontWeight.bold
+                        )),
+                    theme: ExpandableThemeData(iconColor: Colors.blue),
+                    expanded: HabitGroup2(habitList: [
+                      "Quit smoking",
+                      "Quit eating snacks",
+                      "Quit alcohol",
+                      "Stop swearing"
+                    ], yaziTipiRengi: _yaziTipiRengi),
+                    collapsed: Container(),
+                  ))),
+                  color: Colors.red.shade700,
+                ),
+                Center(
+                  child: InkWell(
+                    onTap: () async {
+                      await _auth.signOut();
+          
+                      var a = await _authService.signOut();
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => CheckAuth()));
+                    },
+                    child: Container(
+                      child: Text("Sign Out"),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ));
   }
