53c74a42ec50c6527bf7073ce5a7e28244047544
Anil Yavuz <anielyavuz@gmail.com>
2022-12-24 21:10:37 +0300

    21:10 24.12.2022

--- lib/auth/authFunctions.dart
+++ lib/auth/authFunctions.dart
@@ -1,12 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   Future<Map> anonymSignIn() async {
     Map returnCode = {};
     try {
       var user = await _auth.signInAnonymously();
+
+      await _firestore.collection("Users").doc(user.user!.uid).set(
+          {"registerType": "Anonym", "id": user.user!.uid, "userAuth": "Prod"});
     } on FirebaseAuthException catch (e) {
       returnCode['status'] = false;
       returnCode['value'] = e.code;
